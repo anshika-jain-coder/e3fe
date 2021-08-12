0eb0e0edf231bd2ab89de0b52b05700b437b65b3
anshika-jain-coder <73584808+anshika-jain-coder@users.noreply.github.com>
2021-08-12 22:16:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0
+noseY=0
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/4d1LtppL/114-1147898-clown-nose-png-clip-art-clown-nose-transparent-removebg-preview.png')
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x-25;
+        noseY=results[0].pose.nose.y-12;
+        console.log("nose x ="+ results[0].pose.nose.x);
+        console.log("nose y ="+ results[0].pose.nose.y);
+    }
+}
+function draw(){
+    image(video,0,0,300,300);
+   image(clown_nose,noseX,noseY,40,40);
+   // circle(noseX,noseY,20);
+   // fill(255,0,0); 
+   // stroke (255,0,0);
+
+  }
+    
+
+function take_snapshot(){
+   save('myFilterImage.png');
+}
